bb80cd4387d7a330392387c6a0028d0608a7988f
Maciej Pryc <pryc.maciej@gmail.com>
2018-12-08 20:00:00 +0100

    Import resources from original Resources folder.

--- Deploy.bat
+++ Deploy.bat
@@ -11,12 +11,10 @@ xcopy /s .\proj.win32\Release.win32\*.dll C:\Users\Cerberus\Dropbox\SquareTapper
 xcopy /s .\proj.win32\Release.win32\SquareTapper.exe C:\Users\Cerberus\Dropbox\SquareTapper\%FolderName%\BuildPC
 
 mkdir C:\Users\Cerberus\Dropbox\SquareTapper\%FolderName%\BuildPC\img
-xcopy /s .\proj.win32\Release.win32\img C:\Users\Cerberus\Dropbox\SquareTapper\%FolderName%\BuildPC\img
+xcopy /s .\Resources\img C:\Users\Cerberus\Dropbox\SquareTapper\%FolderName%\BuildPC\img
 mkdir C:\Users\Cerberus\Dropbox\SquareTapper\%FolderName%\BuildPC\fonts
-xcopy /s .\proj.win32\Release.win32\fonts C:\Users\Cerberus\Dropbox\SquareTapper\%FolderName%\BuildPC\fonts
+xcopy /s .\Resources\fonts C:\Users\Cerberus\Dropbox\SquareTapper\%FolderName%\BuildPC\fonts
 mkdir C:\Users\Cerberus\Dropbox\SquareTapper\%FolderName%\BuildPC\lvls
-xcopy /s .\proj.win32\Release.win32\lvls C:\Users\Cerberus\Dropbox\SquareTapper\%FolderName%\BuildPC\lvls
-mkdir C:\Users\Cerberus\Dropbox\SquareTapper\%FolderName%\BuildPC\lvls_demo
-xcopy /s .\proj.win32\Release.win32\lvls_demo C:\Users\Cerberus\Dropbox\SquareTapper\%FolderName%\BuildPC\lvls_demo
+xcopy /s .\Resources\lvls C:\Users\Cerberus\Dropbox\SquareTapper\%FolderName%\BuildPC\lvls
 mkdir C:\Users\Cerberus\Dropbox\SquareTapper\%FolderName%\BuildPC\gui
-xcopy /s .\proj.win32\Release.win32\gui C:\Users\Cerberus\Dropbox\SquareTapper\%FolderName%\BuildPC\gui
+xcopy /s .\Resources\gui C:\Users\Cerberus\Dropbox\SquareTapper\%FolderName%\BuildPC\gui
